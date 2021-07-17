a45528c2f2d9c7d3c39b0f01e9114dd3865e3c85
Pump <74673280+Pump-Codes@users.noreply.github.com>
2021-07-16 23:14:39 -0400

    Create _resource.lua

---
+++ _resource.lua
@@ -0,0 +1,17 @@
+author 'pump'
+description 'dumpsterdiving'
+version "1.0.4"
+
+client_scripts {
+  
+  'client.lua',
+  
+  'config.lua'
+}
+
+server_scripts {
+  'updatecheck.lua',
+  'server.lua',
+ 
+  'config.lua'
+}
